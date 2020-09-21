ab35a73c1962f8fa45af84158803d685c6363534
Rodri Represa <rodrigo@highstreetmobile.com>
2020-09-21 13:40:32 +0200

    Add Quote models

---
+++ app/src/main/java/com/represa/quoty/data/model/Quote.kt
@@ -0,0 +1,21 @@
+package com.represa.quoty.data.model
+
+data class Quote(
+    var id: Int,
+    var dialogue: Boolean,
+    var private: Boolean,
+    var tags: List<String>,
+    var url: String,
+    var author_permalink: String,
+    var favorites_count: Int,
+    var upvotes_count: Int,
+    var downvotes_count: Int,
+    var author: String,
+    var body: String
+)
+
+data class ResponseQuotes(
+    var page: Int,
+    var last_page: Boolean,
+    var quotes: List<Quote>
+)
