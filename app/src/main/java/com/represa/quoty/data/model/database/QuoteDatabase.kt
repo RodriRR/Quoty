d6c218851398494975108c1c00be6d8dbe0d9ee1
Rodri Represa <rodrigo@highstreetmobile.com>
2020-09-22 22:28:35 +0200

    Add sqlite quote entity

---
+++ app/src/main/java/com/represa/quoty/data/model/database/QuoteDatabase.kt
@@ -0,0 +1,24 @@
+package com.represa.quoty.data.model.database
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Quotes")
+data class QuoteDatabase(
+    @PrimaryKey
+    @NonNull
+    var id: Int,
+    var dialogue: Boolean,
+    var hidde: Boolean,
+    var tags: String,
+    var url: String,
+    var author_permalink: String,
+    var favorites_count: Int,
+    var upvotes_count: Int,
+    var downvotes_count: Int,
+    var author: String,
+    var body: String
+)
+
