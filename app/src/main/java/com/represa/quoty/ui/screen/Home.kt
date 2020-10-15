3f2f5ecb94b1e9d7a9c4beb18bfc2526263cd345
Rodri Represa <rodrigo@highstreetmobile.com>
2020-10-15 16:58:46 +0200

    Add transition to Home main text

--- app/src/main/java/com/represa/quoty/ui/screen/Home.kt
+++ app/src/main/java/com/represa/quoty/ui/screen/Home.kt
@@ -1,29 +1,75 @@
 package com.represa.quoty.ui.screen
 
+import androidx.compose.animation.core.FloatPropKey
+import androidx.compose.animation.core.transitionDefinition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.transition
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumnFor
-import androidx.compose.foundation.lazy.LazyRowFor
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.font.font
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.WithConstraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.represa.quoty.R
-import com.represa.quoty.data.model.database.QuoteDatabase
-import com.represa.quoty.ui.components.MainCard
-import com.represa.quoty.ui.components.QuoteCard
+import androidx.ui.tooling.preview.Preview
 import com.represa.quoty.ui.viewmodel.MainViewModel
+import com.represa.quoty.util.ui.AbsoluteAlignment
+
+@Preview
+@Composable
+fun newHome(){
+    Surface(Modifier.fillMaxSize()) {
+        WithConstraints() {
+            Box(){
+
+                var trigger by remember{ mutableStateOf<Boolean>(false) }
+
+                val transition = transition(
+                    definition = remember { searchTransition() },
+                    initState = SearchComponentState.SearchDisabled,
+                    toState = if(trigger){ SearchComponentState.SearchEnabled }else{ SearchComponentState.SearchDisabled}
+                )
+
+                val verticalBias = transition[searchComponentVerticalBias]
+
+                Text(
+                    modifier = Modifier.align(AbsoluteAlignment(verticalBias))
+                        .clickable(onClick = { trigger = !trigger }),
+                    text = "Find Your \nQuote",
+                    fontSize = 28.sp,
+                    lineHeight = 28.sp
+                )
+            }
+        }
+    }
+}
+
+enum class SearchComponentState{
+    SearchEnabled,
+    SearchDisabled
+}
+
+private val searchComponentVerticalBias = FloatPropKey()
+
+private fun searchTransition() = transitionDefinition<SearchComponentState> {
+    //Properties when the search is Disabled
+    state(SearchComponentState.SearchDisabled){
+        this[searchComponentVerticalBias] = 0f
+    }
+    state(SearchComponentState.SearchEnabled){
+        this[searchComponentVerticalBias] = -1f
+    }
+    transition(fromState = SearchComponentState.SearchDisabled, toState = SearchComponentState.SearchEnabled){
+        searchComponentVerticalBias using tween(durationMillis = 1000, delayMillis = 500 )
+    }
+    transition(fromState = SearchComponentState.SearchEnabled, toState = SearchComponentState.SearchDisabled){
+        searchComponentVerticalBias using tween(durationMillis = 1000, delayMillis = 500 )
+    }
+}
+
 
 @Composable
 fun Home(viewModel: MainViewModel) {
@@ -39,26 +85,61 @@ fun Home(viewModel: MainViewModel) {
     )
 }*/
     //}
-    ConstraintLayout(Modifier.padding(0.dp,16.dp,0.dp,0.dp)) {
+    ConstraintLayout(
+        Modifier.padding(0.dp, 16.dp, 0.dp, 0.dp).fillMaxSize()
+    ) {
+
+        var searchMode by remember { mutableStateOf(false) }
+
         // Create references for the composables to constrain
         val (title, subtitle, searchBar, lazyColumn) = createRefs()
 
-        val searchBarConstrains = Modifier.padding(16.dp,0.dp,16.dp,16.dp).constrainAs(searchBar){
-            top.linkTo(title.bottom)
-        }
+        /*val modifierSearch = Modifier.padding(16.dp, 16.dp, 0.dp, 16.dp)
+            .clickable(onClick = { searchMode = false })
+            .constrainAs(title) {
+                top.linkTo(parent.top)
+                absoluteLeft.linkTo(parent.absoluteLeft)
+            }.background(Color.Red)
+
+        val modifierNoSearch = Modifier.padding(16.dp, 16.dp, 0.dp, 16.dp)
+            .clickable(onClick = { searchMode = true })
+            .constrainAs(title) {
+                top.linkTo(parent.top)
+                absoluteLeft.linkTo(parent.absoluteLeft)
+                bottom.linkTo(parent.bottom)
+                absoluteRight.linkTo(parent.absoluteRight)
+            }.background(Color.Cyan)*/
+
+        /*val searchBarConstrains =
+            Modifier.padding(16.dp, 0.dp, 16.dp, 16.dp).constrainAs(searchBar) {
+                top.linkTo(title.bottom)
+            }*/
+        
+
+        val paddingSearch = PaddingValues(16.dp, 16.dp, 0.dp, 16.dp)
+        val paddingNoSearch = PaddingValues(16.dp, 200.dp, 0.dp, 16.dp)
 
         Text(
-            modifier = Modifier.padding(16.dp,16.dp,0.dp,16.dp).constrainAs(title){top.linkTo(parent.top)},
+            modifier = Modifier.padding(if (searchMode) { paddingSearch } else { paddingNoSearch } )
+                .clickable(onClick = { searchMode = !searchMode })
+                .constrainAs(title) {
+                    top.linkTo(parent.top)
+                    absoluteLeft.linkTo(parent.absoluteLeft)
+                },
             text = "Find Your \nQuote",
             fontSize = 28.sp,
             lineHeight = 28.sp
         )
 
-        TopBar(viewModel = viewModel , modifier = searchBarConstrains)
+        /*TopBar(
+            viewModel = viewModel,
+            modifier = searchBarConstrains,
+            onValueChange = { searchMode = true })
 
         Text(
-            modifier = Modifier.padding(16.dp,0.dp,0.dp,16.dp).constrainAs(subtitle){
-                top.linkTo(searchBar.bottom)},
+            modifier = Modifier.padding(16.dp, 0.dp, 0.dp, 16.dp).constrainAs(subtitle) {
+                top.linkTo(searchBar.bottom)
+            },
             text = "Results",
             fontSize = 20.sp
         )
@@ -66,24 +147,26 @@ fun Home(viewModel: MainViewModel) {
         LazyRowFor(
             items = prueba.value,
             itemContent = { QuoteCard(it) },
-            modifier = Modifier.padding(5.dp,0.dp,5.dp,0.dp).constrainAs(lazyColumn){
+            modifier = Modifier.padding(5.dp, 0.dp, 5.dp, 0.dp).constrainAs(lazyColumn) {
                 top.linkTo(subtitle.bottom)
                 bottom.linkTo(parent.bottom)
                 absoluteLeft.linkTo(parent.absoluteLeft)
                 absoluteRight.linkTo(parent.absoluteRight)
             }
-        )
+        )*/
     }
 
 }
 
 
 @Composable
-fun TopBar(viewModel: MainViewModel, modifier: Modifier) {
+fun TopBar(viewModel: MainViewModel, modifier: Modifier, onValueChange: () -> Unit) {
     Column(modifier = modifier) {
         TextField(
             value = viewModel.search,
-            onValueChange = { it -> viewModel.setSearchQuery(it); viewModel.search = it },
+            onValueChange = { it ->
+                viewModel.setSearchQuery(it); viewModel.search = it; onValueChange
+            },
             label = { Text(text = "search") })
     }
 }
